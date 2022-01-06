0e2a78a813f8424b77e52c9647f174333660068e
João Gonçalves <x355505@bcpcorp.net>
2022-01-06 16:56:15 +0000

    Add project files.

---
+++ OrchardCore.Mvc.Module1/Manifest.cs
@@ -0,0 +1,10 @@
+using OrchardCore.Modules.Manifest;
+
+[assembly: Module(
+    Name = "OrchardCore.Mvc.Module1",
+    Author = "The Orchard Core Team",
+    Website = "https://orchardcore.net",
+    Version = "0.0.1",
+    Description = "OrchardCore.Mvc.Module1",
+    Category = "OrchardCore.Mvc.Module1"
+)]
